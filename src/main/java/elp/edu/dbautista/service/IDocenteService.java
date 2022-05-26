113dee851dfb947f18da208b1dfc75d52cbb4401
DannyBautista <bautistadanny276@gmail.com>
2022-05-25 22:40:00 -0500

    Examenv3

--- src/main/java/elp/edu/dbautista/service/IDocenteService.java
+++ src/main/java/elp/edu/dbautista/service/IDocenteService.java
@@ -1,4 +1,13 @@
 package elp.edu.dbautista.service;
 
+import elp.edu.dbautista.entity.Docente;
+
+import java.util.List;
+
 public interface IDocenteService {
+    List<Docente> listaProfesor();
+
+    Docente obtenerDocenteBysalario(String salario);
+
+    List<Docente> findAll();
 }
--- src/main/java/elp/edu/dbautista/service/imple/DocenteServiceImpl.java
+++ src/main/java/elp/edu/dbautista/service/imple/DocenteServiceImpl.java
@@ -1,4 +1,36 @@
 package elp.edu.dbautista.service.imple;
 
-public class DocenteServiceImpl {
+
+import elp.edu.dbautista.entity.Docente;
+import elp.edu.dbautista.service.IDocenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DocenteServiceImpl implements IDocenteService {
+
+    @Autowired
+    private IDocenteService profesorDao;
+
+    @Override
+    public List<Docente> listaProfesor() {
+
+        List<Docente> docenteList = this.profesorDao.findAll();
+        for (Docente docente : docenteList) {
+            System.out.println(docente.toString());
+        }
+        return docenteList;
+    }
+
+    @Override
+    public Docente obtenerDocenteBysalario(String salario) {
+        return null;
+    }
+
+    @Override
+    public List<Docente> findAll() {
+        return null;
+    }
 }
